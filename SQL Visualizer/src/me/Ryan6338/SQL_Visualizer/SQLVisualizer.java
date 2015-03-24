5a1f7b40fc2644bd7a901bd167010ed0c9cfaf8f
Ryan6338 <ryan_marple@hotmail.com>
2015-03-25 08:33:15 +1000

    Update SQLVisualizer.java

--- SQL Visualizer/src/me/Ryan6338/SQL_Visualizer/SQLVisualizer.java
+++ SQL Visualizer/src/me/Ryan6338/SQL_Visualizer/SQLVisualizer.java
@@ -1,7 +1,38 @@
 package me.Ryan6338.SQL_Visualizer;
 
-public class SQLVisualizer {
+import javax.swing.JFrame
+import javax.imageio.ImageIO
+import javax.awt.image.BufferedImage
+import java.io.File
+
+import me.Ryan6338.SQLVisualizer.GUI.StartMenu
+
+public class SQLVisualizer extends JFrame {
 	
+	public SQLVisualizer() {
+		initialize();
+		JPanel startMenu = new StartMenu();
+		this.add(startMenu);
+	}
 	
+	public static void main(String[] args) {
+		new SQLVisualizer();
+	}
+	
+	public BufferedImage getIcon() {
+		File imageFile = new File("icon.jpg")
+		BufferedImage icon = ImageIO.read(imageFile);
+		return icon;
+	}
+	
+	private void initialize() {
+		setTitle("SQL Visualizer");
+		setSize(800, 600);
+		setLocationRelativeTo(null);
+		setResizable(true);
+		setIconImage(getIcon());
+		setDefaultCloseOperation(EXIT_ON_CLOSE);
+		setVisible(true);
+	}
 	
 }
